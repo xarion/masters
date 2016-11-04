7fa64a4434ba7c33460efb3caefb417333babb67
xarion <erdicalli@gmail.com>
2016-11-04 11:40:53 +0100

    moved random generation out of timed code block

--- benchmark.py
+++ benchmark.py
@@ -27,9 +27,9 @@ def main(unused_args):
     with tf.Session(graph=graph) as sess:
         input_tensor = graph.get_tensor_by_name(input_tensor_name)
         output_tensor = graph.get_tensor_by_name(output_tensor_name)
+        random_input = np.random.random_sample(input_shape)
         started_at = time.time()
         for i in xrange(1, number_of_trials):
-            random_input = np.random.random_sample(input_shape)
             sess.run([output_tensor], feed_dict={input_tensor: random_input})
         ended_at = time.time()
         print "Execution took %.2f seconds" % (ended_at - started_at)
