cc7388f537a71386d072db56ee8ea067c1fdc4d2
xarion <erdicalli@gmail.com>
2017-03-28 14:38:12 +0200

    moved some implementation details into conv2d function, removed dropout for a simpler experiment.

--- structures/decomposeme/model.py
+++ structures/decomposeme/model.py
@@ -22,8 +22,10 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d(input_layer, filter_size, in_channels, out_channels, valid_padding=False):
+    W = weight_variable([filter_size, filter_size, in_channels, out_channels])
+    b = bias_variable([out_channels])
+    return tf.nn.conv2d(input_layer, W, strides=[1, 1, 1, 1], padding='SAME' if not valid_padding else 'VALID') + b
 
 
 def max_pool_2x2(x):
@@ -31,33 +33,20 @@ def max_pool_2x2(x):
                           strides=[1, 2, 2, 1], padding='SAME')
 
 
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-
 x_image = tf.reshape(x, [-1, 28, 28, 1])
 
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_conv1 = tf.nn.relu(conv2d(x_image, filter_size=5, in_channels=1, out_channels=32))
 h_pool1 = max_pool_2x2(h_conv1)
 
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_conv2 = tf.nn.relu(conv2d(h_pool1, filter_size=5, in_channels=32, out_channels=64))
 h_pool2 = max_pool_2x2(h_conv2)
 
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
-
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
+h_fc1 = tf.nn.relu(conv2d(h_pool2, filter_size=7, in_channels=64, out_channels=1024, valid_padding=True))
+h_fc1 = tf.reshape(h_fc1, [-1, 1024])
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
 
-y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+y_conv = tf.matmul(h_fc1, W_fc2) + b_fc2
 
 cross_entropy = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
@@ -69,9 +58,9 @@ for i in range(200):
     batch = mnist.train.next_batch(50)
     if i % 100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
-            x: batch[0], y_: batch[1], keep_prob: 1.0})
+            x: batch[0], y_: batch[1]})
         print("step %d, training accuracy %g" % (i, train_accuracy))
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
 print("test accuracy %g" % accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    x: mnist.test.images, y_: mnist.test.labels}))
