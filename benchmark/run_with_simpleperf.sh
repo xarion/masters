9a4028d8da28cc9f730a1c1a6ae9e8a48e8c9ac7
xarion <erdicalli@gmail.com>
2017-04-25 11:02:49 +0200

    changed wait time to 10 seconds to be sure that the benchmark file is written.

--- benchmark/run_with_simpleperf.sh
+++ benchmark/run_with_simpleperf.sh
@@ -35,7 +35,7 @@ adb shell run-as masters.benchmark.android kill -15 $PID
 adb shell am start -n masters.benchmark.android/masters.benchmark.android.BenchmarkActivity
 PID=$(echo $(adb shell ps | grep masters.benchmark.android) | awk {'print $2'})
 adb shell run-as masters.benchmark.android ./simpleperf stat -p $PID -e L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-icache-loads,L1-icache-load-misses,L1-icache-stores,L1-icache-store-misses,dTLB-loads,dTLB-stores,iTLB-loads,iTLB-stores,branch-loads,branch-load-misses,branch-stores,branch-store-misses,node-loads,node-load-misses,node-stores,node-store-misses,node-prefetches,node-prefetch-misses,cpu-cycles,instructions,branch-instructions,branch-misses,bus-cycles,stalled-cycles-frontend,stalled-cycles-backend,cpu-clock,task-clock,page-faults,context-switches,cpu-migrations,minor-faults,major-faults,alignment-faults,emulation-faults --duration 20 > $1.out
-sleep 3
+sleep 10
 adb shell run-as masters.benchmark.android cat $1.log >> $1.out
 adb shell setprop security.perf_harden 1
 
