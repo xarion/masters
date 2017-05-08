7095ba405f62f30a06a4a07d9303b5420c2d2d23
xarion <erdicalli@gmail.com>
2017-05-08 11:03:15 +0200

    modified model to adapt to training codebase

--- model/model.py
+++ model/model.py
@@ -1,17 +1,47 @@
 import tensorflow as tf
 
-from blocks import conv2d, relu, residual_bottleneck_separable, fc
+from blocks import conv2d, relu, residual_bottleneck_separable, normalized_fc, biased_fc
 
 
-class Graph:
-    def __init__(self, input_placeholder):
-        # self.input_placeholder = tf.placeholder(dtype=tf.float32, shape=(None, 224, 224, 3))
-        self.input_placeholder = input_placeholder
-        self.logits = self.inference()
+class SeparableResnet:
+    def __init__(self, learning_rate=None, input_tensor=None, label_tensor=None):
 
-    def inference(self):
+        if input_tensor is None:
+            self.input = tf.placeholder(dtype=tf.float32, shape=(None, 224, 224, 3))
+        else:
+            self.input = input_tensor
+
+        if label_tensor is None:
+            self.label = tf.placeholder(dtype=tf.int32, shape=(None))
+        else:
+            self.label = label_tensor
+
+        preprocessed_input = self.pre_process_input()
+
+        self.freeze_layer = None
+        self.learning_rate = learning_rate
+        self.logits = self.inference(preprocessed_input)
+
+        self.top_1_accuracy = None
+        self.loss = None
+        self.train_step = None
+        self.global_step = None
+        self.one_hot_truth = None
+        self.training()
+        self.evaluation()
+        # dummy variable that is temporarily ignored
+        self.inf = tf.placeholder_with_default(True, None, name='inference')
+        self.keep_prob = tf.placeholder(tf.float32)
+        self.pop_vars = tf.Variable(0)
+
+    def pre_process_input(self):
+        norm = tf.div(self.input, tf.constant(255.0), 'norm')
+        return tf.subtract(norm, tf.constant(0.5), 'trans')
+
+    def inference(self, preprocessed_input):
+
         with tf.variable_scope("conv_1"):
-            conv_1 = conv2d(self.input_placeholder, filter_size=7, input_channels=3, output_channels=64, strides=2)
+            conv_1 = conv2d(preprocessed_input, filter_size=7, input_channels=3, output_channels=64, strides=2)
             relu_1 = relu(conv_1)
 
         with tf.variable_scope("max_pool_1"):
@@ -56,9 +86,38 @@ class Graph:
                                                                strides=2 if residual_block == 1 else 1)
             input_channels = 2048
 
-        with tf.variable_scope("fully_connected"):
+        with tf.variable_scope("fully_connected_1"):
             avg_pool = tf.nn.avg_pool(residual_layer, [1, 7, 7, 1], [1, 1, 1, 1], "VALID")
-            avg_pool = tf.squeeze(avg_pool)
-            logits = fc(avg_pool, input_channels=2048, output_channels=1000)
 
+            avg_pool = tf.squeeze(avg_pool, axis=[1, 2])
+            self.freeze_layer = avg_pool
+
+            fc1 = normalized_fc(avg_pool, input_channels=2048, output_channels=1000)
+        with tf.variable_scope("output"):
+            logits = biased_fc(fc1, input_channels=1000, output_channels=1000)
         return logits
+
+    def training(self):
+        with tf.name_scope('loss'):
+            self.one_hot_truth = tf.squeeze(tf.one_hot(self.label, 1000, on_value=0.9, off_value=0.0001))
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.one_hot_truth)
+            self.loss = tf.reduce_mean(cross_entropy)
+            tf.add_to_collection('losses', self.loss)
+
+        with tf.name_scope('train'):
+            tf.summary.scalar('loss_total', self.loss)
+            if self.learning_rate is not None:
+                optimizer = tf.train.AdamOptimizer(self.learning_rate)
+            else:
+                optimizer = tf.train.AdamOptimizer()
+            self.global_step = tf.Variable(0, name='global_step', trainable=False)
+            self.train_step = optimizer.minimize(self.loss, global_step=self.global_step)
+
+    def evaluation(self):
+        with tf.name_scope('accuracy'):
+            correct_prediction = tf.equal(tf.cast(tf.argmax(self.logits, 1), tf.int32), self.label)
+            correct_prediction_2 = tf.nn.in_top_k(self.logits, self.label, 5, name=None)
+            self.top_1_accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            top_5_accuracy = tf.reduce_mean(tf.cast(correct_prediction_2, tf.float32))
+            tf.summary.scalar('accuracy_top1', self.top_1_accuracy)
+            tf.summary.scalar('accuracy_top5', top_5_accuracy)
