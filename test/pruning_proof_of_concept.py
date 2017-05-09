d6ff8199bc5e0ada277f2f739f6edb5bbda88121
xarion <erdicalli@gmail.com>
2017-05-09 16:54:49 +0200

    full proof of concept, creating a model, pruning weights, saving the checkpoint file, and loading it.

--- test/pruning_proof_of_concept.py
+++ test/pruning_proof_of_concept.py
@@ -55,6 +55,52 @@ class LoadPrunedModel(unittest.TestCase):
             np.testing.assert_array_equal(weights, np.zeros([3, 3, 5, 5]))
             session.close()
 
+    def test_assign_and_load_pruned_weights(self):
+        def get_model():
+            with tf.variable_scope("foo"):
+                v = tf.get_variable("bar", [3, 3, 5, 11], initializer=tf.ones_initializer(), validate_shape=False)
+                conv = tf.nn.conv2d(tf.ones([1, 5, 5, 5]), v, strides=[1, 1, 1, 1], padding="SAME")
+            return v, conv
+
+        def prune(weight_tensor):
+            return tf.assign(weight_tensor, tf.zeros([3, 3, 5, 5]), validate_shape=False)
+
+        g = tf.Graph()
+        with tf.Session(graph=g) as session:
+            weight_tensor, op = get_model()
+            prune_op = prune(weight_tensor)
+            saver = tf.train.Saver()
+            session.run(tf.global_variables_initializer())
+
+            shape, weights, = session.run([tf.shape(op), weight_tensor])
+            np.testing.assert_array_equal(shape, [1, 5, 5, 11])
+            np.testing.assert_array_equal(weights, np.ones([3, 3, 5, 11]))
+
+            _, weights = session.run([prune_op, weight_tensor])
+            weights, = session.run([weight_tensor])
+
+            np.testing.assert_array_equal(weights, np.zeros([3, 3, 5, 5]))
+
+            saver.save(session, CHECKPOINT)
+            session.close()
+
+        # load the checkpoint file for a model with different size
+        starting_checkpoint = tf.train.latest_checkpoint(CHECKPOINT_FOLDER)
+        assert starting_checkpoint is not None
+
+        #  define the starting network but load the pruned kernels with different shapes/values.
+        g = tf.Graph()
+        with tf.Session(graph=g) as session:
+            weight_tensor, op = get_model()
+            saver = tf.train.Saver()
+            saver.restore(session, starting_checkpoint)
+
+            shape, weights, = session.run([tf.shape(op), weight_tensor])
+
+            np.testing.assert_array_equal(shape, [1, 5, 5, 5])
+            np.testing.assert_array_equal(weights, np.zeros([3, 3, 5, 5]))
+            session.close()
+
 
 if __name__ == '__main__':
     unittest.main()
