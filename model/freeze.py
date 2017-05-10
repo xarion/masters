59e70e2b895fca8c1580a4cfeef71c877d5c7c3c
xarion <erdicalli@gmail.com>
2017-05-10 15:30:51 +0200

    modified freeze to load from checkpoint file if there is one.

--- model/freeze.py
+++ model/freeze.py
@@ -1,18 +1,25 @@
 from model import SeparableResnet
 import tensorflow as tf
 
+CHECKPOINT_FOLDER = "checkpoints"
 with tf.Session() as session:
     input_placeholder = tf.placeholder(dtype=tf.float32, shape=(None, 224, 224, 3))
-    graph = SeparableResnet(input_placeholder)
+    graph = SeparableResnet(input_tensor=input_placeholder)
 
-    session.run(tf.variables_initializer(tf.global_variables()))
+    checkpoint = tf.train.latest_checkpoint(CHECKPOINT_FOLDER)
+    if checkpoint:
+        saver = tf.train.Saver()
+        saver.restore(session, checkpoint)
+    else:
+        session.run(tf.variables_initializer(tf.global_variables()))
+
     graph_def = tf.graph_util.convert_variables_to_constants(
         session, session.graph_def, [graph.logits.op.name])
 
     print(graph.logits.op.name)
     print(input_placeholder.name)
 
-    with tf.gfile.GFile("normal_resnet.pb", "wb") as f:
+    with tf.gfile.GFile("separable_resnet.pb", "wb") as f:
         f.write(graph_def.SerializeToString())
     f.close()
     session.close()
