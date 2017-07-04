fb9f71aa145a8cac8b25e9e045490db51210fa54
xarion <erdicalli@gmail.com>
2017-07-04 13:07:06 +0200

    added a "resave" step to account for tensorflow version changes in model.cifar.freeze.

--- model/cifar/freeze.py
+++ model/cifar/freeze.py
@@ -1,18 +1,28 @@
-from separable_resnet import SeparableResnet
 import tensorflow as tf
+
+from separable_resnet import SeparableResnet
 
 CHECKPOINT_FOLDER = "checkpoints"
 with tf.Session() as session:
     # input_placeholder = tf.placeholder(dtype=tf.float32, shape=(None, 224, 224, 3))
     graph = SeparableResnet(training=False)
 
-    # checkpoint = tf.train.latest_checkpoint(CHECKPOINT_FOLDER)
-    # if checkpoint:
-    #     saver = tf.train.Saver()
-    #     saver.restore(session, checkpoint)
-    # else:
-    #     session.run(tf.variables_initializer(tf.global_variables()))
-    session.run(tf.variables_initializer(tf.global_variables()))
+    checkpoint = tf.train.latest_checkpoint(CHECKPOINT_FOLDER)
+    saver = tf.train.Saver()
+    if checkpoint:
+        saver.restore(session, checkpoint)
+    else:
+        session.run(tf.variables_initializer(tf.global_variables()))
+    saver.save(session, CHECKPOINT_FOLDER+ "/separable_resnet-cifar-10-resaved", global_step=0)
+    session.close()
+
+with tf.Session() as session:
+    checkpoint = tf.train.latest_checkpoint(CHECKPOINT_FOLDER)
+
+    if checkpoint:
+        saver.restore(session, checkpoint)
+    else:
+        session.run(tf.variables_initializer(tf.global_variables()))
 
     graph_def = tf.graph_util.convert_variables_to_constants(
         session, session.graph_def, [graph.logits.op.name])
@@ -20,7 +30,7 @@ with tf.Session() as session:
     print(graph.logits.op.name)
     print(graph.input.name)
 
-    with tf.gfile.GFile("../../benchmark/models/separable_resnet-ehex.pb", "wb") as f:
+    with tf.gfile.GFile("separable_resnet-cifar-10.pb", "wb") as f:
         f.write(graph_def.SerializeToString())
     f.close()
     session.close()
